acf287a5f00e8121642c8f1522fd3c93d4effb9a
Benoît Petitcollot <bpetitcollot@yahoo.fr>
2025-02-16 23:04:37 +0100

    select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,49 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+const SelectElement = styled.select`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 16px;
+    padding: 12px 16px;
+    opacity: 0;
+`;
+const SelectedElement = styled.div`
+    display: inline-block;
+  font-size: 16px;
+  padding: 12px 16px;
+  background-color: ${COLORS.transparentGray15};
+  border: 0;
+  border-radius: 8px;
+  white-space: nowrap;
+`;
+
+const Caret = styled(Icon)`
+    display: inline-block;
+    height: 1em;
+    margin-left: 1em;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+      <Wrapper>
+          <SelectedElement>
+              {displayedValue}
+              <Caret id="chevron-down" />
+          </SelectedElement>
+          <SelectElement value={value} onChange={onChange}>
+            {children}
+        </SelectElement>
+      </Wrapper>
   );
 };
 
